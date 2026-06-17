ba3b9caa25e8e305e59a9b719c11d272c16d9013
Javier Arias <javier@jarias.org>
2026-06-17 14:15:16 +0900

    Change viewport

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const TimelineTab = lazy(() =>
 	})),
 );
 
-const textStyles = 'hidden @md:block';
+const textStyles = 'hidden @xs:block';
 
 function App({ doi }: { doi: Doi }) {
 	const isValid = isValidDoi(doi);
